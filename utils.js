90d5fd1b0a8e33fa2dc2ae39412f9ca094dc63f5
mojovsky <danielderewonko@gmail.com>
2024-06-12 20:58:25 +0100

    cm

--- utils.js
+++ utils.js
@@ -1,7 +1,10 @@
-function getRandomQuote() {
-  return fetch("http://api.quotable.io/random")
-    .then((response) => response.json())
-    .then((data) => data.content);
-}
-
-export { getRandomQuote };
+const getRenderedQuote = async () => {
+  const response = await fetch("http://api.quotable.io/random");
+  const data = await response.json();
+  const quote = data.content;
+  const arr = quote.split("").map((value) => {
+    return `<span class="quote-chars">${value}</span>`;
+  });
+  const quoteSectionHTML = arr.join("");
+  return quoteSectionHTML;
+};
