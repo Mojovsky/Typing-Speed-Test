5fb2e25786063245839aad8090ca4e5be2313145
mojovsky <danielderewonko@gmail.com>
2024-06-15 12:52:54 +0100

    semantic

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,8 @@
 import { getRandomWord, formatWord, addClass } from "./words.js";
+import { getWpm, getWordAccuracy } from "./stat-logic.js";
 import { handleKeyUp } from "./key.js";
 
-async function runGame() {
+async function newTest() {
   document.getElementById("words").innerHTML = "";
   for (let i = 0; i < 200; i++) {
     let word = await getRandomWord();
@@ -16,4 +17,4 @@ async function runGame() {
   });
 }
 
-runGame();
+newTest();
