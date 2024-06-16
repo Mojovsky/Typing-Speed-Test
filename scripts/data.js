2dde2f15d1a0037e6fb7bfdb041c8ef15e6296a3
mojovsky <danielderewonko@gmail.com>
2024-06-16 20:17:16 +0100

    logic for storing data and calculating avg

---
+++ scripts/data.js
@@ -0,0 +1,46 @@
+function updateElementText(elementId, text) {
+  document.getElementById(elementId).innerHTML = text;
+}
+
+const getElementText = (elementId) =>
+  document.getElementById(elementId).innerHTML;
+
+function storeData() {
+  const tests = (parseInt(localStorage.getItem("tests")) || 0) + 1;
+  const wpm = localStorage.getItem("wpm")
+    ? (parseFloat(localStorage.getItem("wpm")) * tests +
+        parseFloat(getElementText("wpm-value"))) /
+      (tests + 1)
+    : parseFloat(getElementText("wpm-value"));
+  const accuracy = localStorage.getItem("accuracy")
+    ? (parseFloat(localStorage.getItem("accuracy")) * tests +
+        parseFloat(getElementText("accuracy-value").replace("%", ""))) /
+      (tests + 1)
+    : parseFloat(getElementText("accuracy-value").replace("%", ""));
+  const errors = localStorage.getItem("errors")
+    ? (parseInt(localStorage.getItem("errors")) * tests +
+        parseInt(getElementText("errors-value"))) /
+      (tests + 1)
+    : parseInt(getElementText("errors-value"));
+
+  updateElementText("tests", tests);
+  updateElementText("avg-wpm", wpm.toFixed(0));
+  updateElementText("avg-accuracy", accuracy.toFixed(0));
+  updateElementText("avg-errors", errors.toFixed(0));
+
+  localStorage.setItem("tests", tests);
+  localStorage.setItem("wpm", wpm);
+  localStorage.setItem("accuracy", accuracy);
+  localStorage.setItem("errors", errors);
+}
+
+function getStoredData() {
+  return {
+    tests: localStorage.getItem("tests") || 0,
+    wpm: localStorage.getItem("wpm") || 0,
+    accuracy: localStorage.getItem("accuracy") || 0,
+    errors: localStorage.getItem("errors") || 0,
+  };
+}
+
+export { storeData, getStoredData };
