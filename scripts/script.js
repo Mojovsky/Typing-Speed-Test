cce6f291b065269f4d1b43b01e326e55871081eb
mojovsky <danielderewonko@gmail.com>
2024-06-16 16:51:33 +0100

    refactor

--- scripts/script.js
+++ scripts/script.js
@@ -1,40 +1,49 @@
 import { displayWords } from "./words.js";
 import { getStats } from "./stat-logic.js";
 import { inputValidation } from "./validation.js";
-import { startTimer, resetTimer } from "./timer.js";
 
-let gameState = false;
+let timerId;
+
+async function main() {
+  await displayWords();
+  document.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+      runTest();
+    }
+  });
+}
+
+async function runTest() {
+  await displayWords();
+  newTest();
+  timerId = await startTimer(20);
+  getStats();
+}
 
 async function newTest() {
   document.querySelector(".word").classList.add("current");
   document.querySelector(".letter").classList.add("current");
 
-  document.addEventListener("keyup", (event) => {
+  document.addEventListener("keydown", (event) => {
     if (event.target === document.body) {
       inputValidation(event);
     }
   });
 }
 
-async function runTest() {
-  gameState = true;
-  newTest();
-  await startTimer(20);
-  getStats();
-  gameState = false;
-}
-
-async function main() {
-  await displayWords();
-  document.addEventListener("keydown", handleKeyDown);
-}
-
-async function handleKeyDown(event) {
-  if (event.key === "Enter") {
-    resetTimer();
-    await displayWords();
-    await runTest();
-  }
+async function startTimer(seconds) {
+  return new Promise((resolve) => {
+    let timer = seconds;
+    const interval = setInterval(() => {
+      timer--;
+      document.getElementById("timer").innerHTML = `<span>${timer}s</span>`;
+      if (timer <= 0) {
+        clearInterval(interval);
+        resolve();
+      }
+    }, 1000);
+    return interval;
+  });
 }
 
 main();
